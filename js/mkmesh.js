fae26d9a76436e3874eee139d50a7038b205d007
Marco Biasini <marco.biasini@unibas.ch>
2013-02-16 11:00:33 +0100

    initial commit

---
+++ js/mkmesh.js
@@ -0,0 +1,18 @@
+// generates a mesh from the given calpha positions
+function make_mesh(calpha_positions) {
+    var extrude_settings = { steps : calpha_positions.length*10,
+                             radius : 0.7 }
+   var spline_data = [];
+   for (var  i = 0; i < calpha_positions.length; ++i) {
+     spline_data.push(new THREE.Vector3(calpha_positions[i][0],
+                                        calpha_positions[i][1],
+                                        calpha_positions[i][2]));
+   }
+   var spline = new THREE.SplineCurve3(spline_data);
+
+   var tube_mesh = new THREE.TubeGeometry(spline,
+                                          extrude_settings.steps,
+                                          extrude_settings.radius,
+                                          5, false, true);
+   return tube_mesh;
+}
