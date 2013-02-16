0ef08b7ef0c4b4285e67cd5f1e5fadebccf9dfbf
Marco Biasini <marco.biasini@unibas.ch>
2013-02-16 12:29:43 +0100

    first version of protein viewer

--- js/mkmesh.js
+++ js/mkmesh.js
@@ -1,7 +1,7 @@
 // generates a mesh from the given calpha positions
 function make_mesh(calpha_positions) {
     var extrude_settings = { steps : calpha_positions.length*10,
-                             radius : 0.7 }
+                             radius : 0.4 }
    var spline_data = [];
    for (var  i = 0; i < calpha_positions.length; ++i) {
      spline_data.push(new THREE.Vector3(calpha_positions[i][0],
@@ -13,6 +13,6 @@ function make_mesh(calpha_positions) {
    var tube_mesh = new THREE.TubeGeometry(spline,
                                           extrude_settings.steps,
                                           extrude_settings.radius,
-                                          5, false, true);
+                                          12, false, true);
    return tube_mesh;
 }
