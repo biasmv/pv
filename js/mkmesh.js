c7c8d5e67b30f72d2ef150bd459082eb8af48175
Marco Biasini <mvbiasini@gmail.com>
2013-02-23 23:35:44 +0100

    outline shading

--- js/mkmesh.js
+++ js/mkmesh.js
@@ -1,7 +1,7 @@
 // generates a mesh from the given calpha positions
 function make_mesh(calpha_positions) {
     var extrude_settings = { steps : calpha_positions.length*10,
-                             radius : 0.7 }
+                             radius : 0.4 }
    var spline_data = [];
    for (var  i = 0; i < calpha_positions.length; ++i) {
      spline_data.push(new THREE.Vector3(calpha_positions[i][0],
@@ -13,7 +13,7 @@ function make_mesh(calpha_positions) {
    var tube_mesh = new THREE.TubeGeometry(spline,
                                           extrude_settings.steps,
                                           extrude_settings.radius,
-                                          12, false, false);
+                                          24, false, false);
    return tube_mesh;
 }
 
