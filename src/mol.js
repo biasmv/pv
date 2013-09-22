87eeed5a41d78bbfa7bdcef2e7afb4d85453b719
Marco Biasini <marco.biasini@unibas.ch>
2013-09-22 08:58:15 +0200

    cleanup

--- src/mol.js
+++ src/mol.js
@@ -4,21 +4,23 @@
 
 
 
+// atom covalent radii by element derived from Cambrige Structural Database. 
+// Source: http://profmokeur.ca/chemistry/covalent_radii.htm
 var ELEMENT_COVALENT_RADII = {
-H : .31, HE : .28, LI : 1.28, BE : .96, B : .84, C : .76, N : .71, O : .66,
-F : .57, NE : .58, NA : 1.66, MG : 1.41, AL : 1.21, SI : 1.11, P : 1.07,
-S : 1.05, CL : 1.02, AR : 1.06, K : 2.03, CA : 1.76, SC : 1.70, TI : 1.60,
-V : 1.53, CR : 1.39, MN : 1.39, FE : 1.32, CO : 1.26, NI : 1.24, CU : 1.32,
-ZN : 1.22, GA : 1.22, GE : 1.20, AS : 1.19, SE : 1.20, BR : 1.20, KR : 1.16,
-RB : 2.20, SR : 1.95, Y : 1.90, ZR : 1.75, NB : 1.64, MO : 1.54, TC : 1.47,
-RU : 1.46, RH : 1.42, PD : 1.39, AG : 1.45, CD : 1.44, IN : 1.42, SN : 1.39,
-SB : 1.39, TE : 1.38, I : 1.39, XE : 1.40, CS : 2.44, BA : 2.15, LA : 2.07,
-CE : 2.04, PR : 2.03, ND : 2.01, PM : 1.99, SM : 1.98, EU : 1.98, GD : 1.96,
-TB : 1.94, DY : 1.92, HO : 1.92, ER : 1.89, TM : 1.90, YB : 1.87, LU : 1.87,
-HF : 1.75, TA : 1.70, W : 1.62, RE : 1.51, OS : 1.44, IR : 1.41, PT : 1.36,
-AU : 1.36, HG : 1.32, TL : 1.45, PB : 1.46, BI : 1.48, PO : 1.40, AT : 1.50, 
-RN : 1.50, FR : 2.60, RA : 2.21, AC : 2.15, TH : 2.06, PA : 2.00, U : 1.96,
-NP : 1.90, PU : 1.87, AM : 1.80, CM : 1.69
+ H : 0.31, HE : 0.28, LI : 1.28, BE : 0.96,  B : 0.84,  C : 0.76,  N : 0.71, 
+ O : 0.66,  F : 0.57, NE : 0.58, NA : 1.66, MG : 1.41, AL : 1.21, SI : 1.11, 
+ P : 1.07,  S : 1.05, CL : 1.02, AR : 1.06,  K : 2.03, CA : 1.76, SC : 1.70, 
+TI : 1.60,  V : 1.53, CR : 1.39, MN : 1.39, FE : 1.32, CO : 1.26, NI : 1.24, 
+CU : 1.32, ZN : 1.22, GA : 1.22, GE : 1.20, AS : 1.19, SE : 1.20, BR : 1.20, 
+KR : 1.16, RB : 2.20, SR : 1.95,  Y : 1.90, ZR : 1.75, NB : 1.64, MO : 1.54, 
+TC : 1.47, RU : 1.46, RH : 1.42, PD : 1.39, AG : 1.45, CD : 1.44, IN : 1.42, 
+SN : 1.39, SB : 1.39, TE : 1.38,  I : 1.39, XE : 1.40, CS : 2.44, BA : 2.15, 
+LA : 2.07, CE : 2.04, PR : 2.03, ND : 2.01, PM : 1.99, SM : 1.98, EU : 1.98, 
+GD : 1.96, TB : 1.94, DY : 1.92, HO : 1.92, ER : 1.89, TM : 1.90, YB : 1.87, 
+LU : 1.87, HF : 1.75, TA : 1.70,  W : 1.62, RE : 1.51, OS : 1.44, IR : 1.41, 
+PT : 1.36, AU : 1.36, HG : 1.32, TL : 1.45, PB : 1.46, BI : 1.48, PO : 1.40, 
+AT : 1.50, RN : 1.50, FR : 2.60, RA : 2.21, AC : 2.15, TH : 2.06, PA : 2.00, 
+ U : 1.96, NP : 1.90, PU : 1.87, AM : 1.80, CM : 1.69
 };
 
 function covalentRadius(ele) {
@@ -90,7 +92,7 @@ MolBase.prototype.selectWithin = (function() {
   return function(mol, options) {
     console.time('Mol.selectWithin');
     options = options || {};
-    var radius = options.radius || 4.0
+    var radius = options.radius || 4.0;
     var radiusSqr = radius*radius;
     var matchResidues = !!options.matchResidues;
     var targetAtoms = [];
@@ -134,7 +136,7 @@ MolBase.prototype.selectWithin = (function() {
     }
     console.timeEnd('Mol.selectWithin');
     return view;
-  }
+  };
 })();
 
 function ChainBase() {
