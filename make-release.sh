fcb6e1337d212215c7dc2861b63b87cf58dc4158
Marco Biasini <mvbiasini@gmail.com>
2014-10-17 19:58:26 +0200

    update release script

--- make-release.sh
+++ make-release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
 grunt
 RELEASE_VERSION=`git tag | tail -n 1`
 DIR="pv-$RELEASE_VERSION"
 echo $DIR
 rm -rf $DIR
 mkdir $DIR
-cp js/pv.js $DIR/pv-$RELEASE_VERSION.js
+cp js/pv.dbg.js $DIR/pv-$RELEASE_VERSION.dbg.js
+cp js/pv.rel.js $DIR/pv-$RELEASE_VERSION.rel.js
 cp js/pv.min.js $DIR/pv-$RELEASE_VERSION.min.js
 cp LICENSE.txt $DIR
 rm -rf $DIR.zip
