6d4ed480ac1690772fc5c5c7c0c2144f46f40974
Marco Biasini <mvbiasini@gmail.com>
2014-12-02 22:30:02 +0100

    get touch working on Android
    The patch is based on the work by @kozmad. It unifies the Android and
    iOS implementation to avoid code duplication. I've only tested this
    on an iPad, but it should also work on Android.

--- src/touch.js
+++ src/touch.js
@@ -22,140 +22,82 @@
 "use strict";
 
 function TouchHandler(element, viewer, cam) {
-	if( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
-		this._element = element;
-		this._element.addEventListener('touchmove', bind(this, this._touchMove));
-		this._element.addEventListener('touchstart', bind(this, this._touchStart));
-		this._element.addEventListener('touchend', bind(this, this._touchEnd));
-		this._element.addEventListener('touchcancel', bind(this, this._touchEnd));
-		this._touchState = {
-		  scale : 1.0,
-		  rotation : 0.0,
-		  center : null,
-		  lastSingleTap : null
-		};
-		this._viewer = viewer;
-		this._cam = cam;
-	}
-	else {
-		this._element = element;
-		this._element.addEventListener('touchmove', bind(this, this._touchMoveAndroid));
-		this._element.addEventListener('touchstart', bind(this, this._touchStartAndroid));
-		this._element.addEventListener('touchend', bind(this, this._touchEndAndroid));
-		this._element.addEventListener('touchcancel', bind(this, this._touchEnd));
-		this._touchState = {
-		  scale : 1.0,
-		  rotation : 0.0,
-		  center : null,
-		  lastSingleTap : null
-		};
-		this._viewer = viewer;
-		this._cam = cam;
-	}
+  this._element = element;
+  this._element.addEventListener('touchmove', bind(this, this._touchMove));
+  this._element.addEventListener('touchstart', bind(this, this._touchStart));
+  this._element.addEventListener('touchend', bind(this, this._touchEnd));
+  this._element.addEventListener('touchcancel', bind(this, this._touchEnd));
+  this._touchState = {
+    scale : 1.0,
+    rotation : 0.0,
+    center : null
+  };
+  this._lastSingleTap = null;
+  this._viewer = viewer;
+  this._cam = cam;
 }
 
-TouchHandler.prototype._touchStartAndroid = function(event) {
-  event.preventDefault();
-  if (event.touches.length === 1) {
-    // detect double tap
-    var now = new Date().getTime();
-    if (this._touchState.lastSingleTap !== null) {
-      var delta = now - this._touchState.lastSingleTap;
-      if (delta < 500) {
-        this._viewer._mouseDoubleClick({ 
-            clientX : event.touches[0].clientX, 
-            clientY : event.touches[0].clientY });
-      }
-    }
-    this._touchState.lastSingleTap = now;
+TouchHandler.prototype._extractEventAttributes = function(previousState, event) {
+  var state = {}
+  state.center = getCenter(event.targetTouches);
+  state.pointers = [];
+  for (var i = 0; i < event.targetTouches.length; ++i) {
+    var t = event.targetTouches[i];
+    state.pointers.push({ x: t.clientX, y : t.clientY });
+  }
+  state.numTouches = event.targetTouches.length;
+  state.rotation = 0;
+  state.scale = 1.0;
+  state.deltaScale = 0.0;
+  state.deltaRotation = 0.0;
+
+  if (previousState.center) {
+    state.deltaCenter = {
+      x : state.center.x - previousState.center.x, 
+      y : state.center.y - previousState.center.y
+    };
+  }
+
+  if (previousState.numTouches !== 2 || state.numTouches !== 2) {
+    return state;
+  }
+  if (previousState.initialPointers) {
+    state.initialPointers = previousState.initialPointers;
   } else {
-    this._touchState.lastSingleTap = null;
+    state.initialPointers = previousState.pointers;
   }
-  this._lastTouchPos = event.touches;
-  return;
+
+  state.scale = getScale(state.initialPointers, state.pointers);
+  state.deltaScale = state.scale - previousState.scale;
+  state.rotation = getRotationAngle(state.initialPointers, state.pointers);
+  state.deltaRotation = state.rotation - previousState.rotation;
+  return state;
 };
 
-TouchHandler.prototype._touchMoveAndroid = function(event) {
-  event.preventDefault();
-  var newTouchPos = event.changedTouches;
-  if( event.touches.length==1 ) {
-    var delta = {
-      x : newTouchPos[0].pageX - this._lastTouchPos[0].pageX,
-      y : newTouchPos[0].pageY - this._lastTouchPos[0].pageY
-    };
-    var speed = 0.005;
-    this._cam.rotateX(speed * delta.y);
-    this._cam.rotateY(speed * delta.x);
-  }
-  else if( event.touches.length==2 ) {
-    //translate
-    var delta = {
-      x : (newTouchPos[0].pageX - this._lastTouchPos[0].pageX + newTouchPos[1].pageX - this._lastTouchPos[1].pageX)/2,
-      y : (newTouchPos[0].pageY - this._lastTouchPos[0].pageY + newTouchPos[1].pageY - this._lastTouchPos[1].pageY)/2
-    };
-    var speed = 0.05;
-    this._cam.panXY(speed * delta.x, speed * delta.y);
-
-    // spin
-    var beg_slope = Math.atan2(this._lastTouchPos[0].pageX-this._lastTouchPos[1].pageX, this._lastTouchPos[0].pageY-this._lastTouchPos[1].pageY);
-    var curr_slope = Math.atan2(newTouchPos[0].pageX-newTouchPos[1].pageX, newTouchPos[0].pageY-newTouchPos[1].pageY);
-    var delta_slope = curr_slope - beg_slope;
-    this._cam.rotateZ(delta_slope);
-
-    // zoom
-    var ox = this._lastTouchPos[0].pageX-this._lastTouchPos[1].pageX;
-    var nx = newTouchPos[0].pageX-newTouchPos[1].pageX;
-    var oy = this._lastTouchPos[0].pageY-this._lastTouchPos[1].pageY;
-    var ny = newTouchPos[0].pageY-newTouchPos[1].pageY;
-    var od = Math.sqrt(ox*ox+oy*oy);
-    var nd = Math.sqrt(nx*nx+ny*ny);
-    var delta =  (od-nd)/od;
-    speed = 0.05																																								
-    this._cam.zoom( delta*speed );
-  }
-  this._lastTouchPos = event.touches;
-	this._viewer.requestRedraw();
-};
 
 TouchHandler.prototype._touchMove = function(event) {
   event.preventDefault();
-  var newCenter = getCenter(event.targetTouches);
-  var deltaCenter = { 
-      x : newCenter.x - this._touchState.center.x,
-      y : newCenter.y - this._touchState.center.y 
-  };
-  // only handle pinch/pan we one target touch in a previous frame. 
-  // Otherwise the calculation of the center might be off which leads
-  // to visible jumps.
-  if (event.targetTouches.length === 2 && 
-      this._touchState.numTouches === 2) { 
-    var deltaScale = (this._touchState.scale - event.scale) * 4.0;
-    if (deltaScale !== 0) {
-      this._cam.zoom(deltaScale);
-    }
-    var deltaZRotation = 
-      Math.PI * (this._touchState.rotation - event.rotation) / 180.0;
-    this._cam.panXY(deltaCenter.x * 0.05, deltaCenter.y * 0.05);
-    this._cam.rotateZ(deltaZRotation);
-    this._viewer.requestRedraw();
+  var newState = this._extractEventAttributes(this._touchState, event);
+  var deltaScale =  - newState.deltaScale * 4.0;
+  if (deltaScale !== 0) {
+    this._cam.zoom(deltaScale);
   }
-  // only handle XZ rotation when we one target touch in a previous frame. 
-  // Otherwise the calculation of the center might be off which leads
-  // to visible jumps.
-  if (event.targetTouches.length === 1 && 
-      this._touchState.numTouches === 1) {
-    // FIXME: ideally we would rotate the scene around the touch center.
-    // This would feel more natural. Now when the touch center is far
-    // away from the project center of the viewer, rotation is a little
-    // awkward.
-    this._cam.rotateX(deltaCenter.y * 0.002);
-    this._cam.rotateY(deltaCenter.x * 0.002);
-    this._viewer.requestRedraw();
+  if (newState.numTouches === 2 && this._touchState.numTouches === 2) {
+    this._cam.panXY(newState.deltaCenter.x * 0.001 * this._cam.zoom(),
+                    newState.deltaCenter.y * 0.001 * this._cam.zoom())
   }
-  this._touchState.center = newCenter;
-  this._touchState.scale = event.scale;
-  this._touchState.rotation = event.rotation;
-  this._touchState.numTouches = event.targetTouches.length;
+  var deltaZRotation =  - newState.deltaRotation;
+  this._cam.rotateZ(deltaZRotation);
+  // FIXME: ideally we would rotate the scene around the touch center.
+  // This would feel more natural. Now when the touch center is far
+  // away from the project center of the viewer, rotation is a little
+  // awkward.
+  if (newState.numTouches === 1 && this._touchState.numTouches === 1) {
+      this._cam.rotateX(newState.deltaCenter.y * 0.005); 
+      this._cam.rotateY(newState.deltaCenter.x * 0.005);
+  }
+  this._viewer.requestRedraw();
+  this._touchState = newState;
 };
 
 function getCenter(touches) {
@@ -169,27 +111,49 @@ function getCenter(touches) {
   return { x : centerX, y : centerY };
 }
 
+function distance(a, b) {
+  var dx = b.x - a.x;
+  var dy = b.y - a.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function getScale(prevPointers, newPointers) {
+  var prevD = distance(prevPointers[0], prevPointers[1]);
+  var newD = distance(newPointers[0], newPointers[1]);
+  return newD / (prevD === 0 ? 1 : prevD);
+}
+
+function getAngle(p1, p2) {
+  var dx = p2.x - p1.x;
+  var dy = p2.y - p1.y;
+  return Math.atan2(dy, dx);
+}
+
+function getRotationAngle(prevPointers, newPointers) {
+  return getAngle(newPointers[1], newPointers[0]) - 
+         getAngle(prevPointers[1], prevPointers[0]);
+}
+
+
 TouchHandler.prototype._touchStart = function(event) {
   event.preventDefault();
   if (event.targetTouches.length === 1) {
     // detect double tap
     var now = new Date().getTime();
-    if (this._touchState.lastSingleTap !== null) {
-      var delta = now - this._touchState.lastSingleTap;
+    if (this._lastSingleTap !== null) {
+      var delta = now - this._lastSingleTap;
       if (delta < 500) {
         this._viewer._mouseDoubleClick({ 
             clientX : event.targetTouches[0].clientX, 
             clientY : event.targetTouches[0].clientY });
       }
     }
-    this._touchState.lastSingleTap = now;
+    this._lastSingleTap = now;
   } else {
-    this._touchState.lastSingleTap = null;
+    this._lastSingleTap = null;
   }
-  this._touchState.scale = event.scale;
-  this._touchState.rotation = event.rotation;
-  this._touchState.center = getCenter(event.targetTouches);
-  this._touchState.numTouches = event.targetTouches.length;
+  this._touchState = 
+    this._extractEventAttributes(this._touchState, event);
 };
 
 TouchHandler.prototype._touchEnd = function(event) {
