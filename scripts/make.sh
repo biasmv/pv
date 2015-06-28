e483659ba6bb73c0a5863b2bef17d2e0e8312d75
Marco Biasini <mvbiasini@gmail.com>
2015-06-28 10:05:06 +0200

    make sure release zip also contains the browseried source

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,5 +2,6 @@
 set -e
 grunt
 cp js/bio-pv.dbg.js bio-pv.js
+npm run-script build-biojs
 echo 'writing mangled output to bio-pv.min.js'
 python scripts/mangle-private-identifiers.py js/bio-pv.min.js bio-pv.min.js
