58ce0fee5a34d0b609c41a9fc24c3851ce431095
Marco Biasini <mvbiasini@gmail.com>
2015-06-30 20:31:31 +0200

    add bower.json to release package

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+
+function write_bower_json {
+  cat <<EOF
+  {
+    "name": "bio-pv",
+    "homepage": "https://github.com/biasmv/pv",
+      "authors": [
+      "Marco Biasini <mvbiasini@gmail.com>"
+    ],
+    "description": "WebGL protein viewer",
+    "main": "bio-pv.min.js",
+    "moduleType": [
+      "amd"
+    ],
+    "keywords": [
+      "protein",
+      "3d",
+      "webgl",
+      "biojs"
+    ],
+    "license": "MIT",
+    "ignore": [
+      "**/.*",
+      "node_modules",
+      "pdbs",
+      "test",
+      "tests"
+    ]
+  }
+EOF
+}
+
+write_bower_json > bower.json
+
 set -e
 grunt
 cp js/bio-pv.dbg.js bio-pv.js
